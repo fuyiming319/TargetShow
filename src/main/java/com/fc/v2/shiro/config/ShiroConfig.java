e6aa1114543f576bc2d69c5d2e56fb1478641f87
开源oschina <115889198@qq.com>
2021-04-20 23:56:03 +0800

    记住我

--- src/main/java/com/fc/v2/shiro/config/ShiroConfig.java
+++ src/main/java/com/fc/v2/shiro/config/ShiroConfig.java
@@ -113,23 +113,6 @@ public class ShiroConfig {
 		return shiroSession;
 	}
 	
-	
-	/**
-	 * 记住我
-	 */
-	@Bean
-    public SimpleCookie rememberMeCookie() {
-        //这个参数是cookie的名称，对应前端的checkbox的name = rememberMe
-        SimpleCookie simpleCookie = new SimpleCookie("rememberMe");
-        //如果httyOnly设置为true，则客户端不会暴露给客户端脚本代码，使用HttpOnly cookie有助于减少某些类型的跨站点脚本攻击；
-        simpleCookie.setHttpOnly(true);
-        // 设置记住7天登陆
-        simpleCookie.setMaxAge(604800);
-        // 设置路径
-        simpleCookie.setPath("/admin/index");
-        return simpleCookie;
-    }
-	
 	/**
 	 * 加密算法
 	 * 
